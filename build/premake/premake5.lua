43f37dfc46100483491ffcc94a10c9052e724c90
Doug Hodson <doug@openeaagles.org>
2018-04-10 14:32:03 -0400

    updated premake file

--- build/premake/premake5.lua
+++ build/premake/premake5.lua
@@ -4,6 +4,7 @@
 -- Target of interest:
 --     vs2015     (Visual Studio 2015)
 --     vs2017     (Visual Studio 2017)
+--     gmake      (gcc / clang)
 --
 
 -- we must have an ide/compiler specified
@@ -34,27 +35,22 @@ workspace "adevs-test"
    --
    configurations { "Release", "Debug" }
 
-   -- visual studio options and warnings
-   -- /wd4351 (C4351 warning) - disable warning associated with array brace initialization
-   -- /wd4996 (C4996 warning) - disable deprecated declarations
-   -- /wd4005 (C4005 warning) - disable macro redefinition
-   -- /wd4100 (C4100 warning) - disable unreferenced formal parameter
-   -- /Oi - generate intrinsic functions
-   --disablewarnings { "4351",  "4996", "4005", "4100" }
-   --buildoptions { "/Oi" }
-
    -- common release configuration flags, symbols and libraries
    filter { "configurations:Release" }
       optimize "On"
-      -- favor speed over size
-      buildoptions { "/Ot" }
-      defines { "WIN32", "NDEBUG" }
+      if _ACTION ~= "gmake" then
+         -- favor speed over size
+         buildoptions { "/Ot" }
+         defines { "WIN32", "NDEBUG" }
+      end
 
    -- common debug configuration flags, symbols and libraries
    filter { "configurations:Debug" }
       symbols "On"
       targetsuffix "_d"
-      defines { "WIN32", "_DEBUG" }
+      if _ACTION ~= "gmake" then
+         defines { "WIN32", "_DEBUG" }
+      end
 
    -- examples
    dofile "examples.lua"
--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -5,6 +5,10 @@ rmdir /S /Q ..\vs2015
 rmdir /S /Q ..\vs2017
 rmdir /S /Q ..\obj
 rem
+rem delete old compiled libraries
+rem
+del ..\..\lib\*.lib
+rem
 rem create new solution/workspace files
 rem
 premake5-alpha12.exe --os=windows --file=premake5.lua vs2015
