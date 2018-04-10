3e4e3ebdbb5f07486e1342c3b5d10a4abc6f9935
Doug Hodson <doug@openeaagles.org>
2018-04-10 15:40:31 -0400

    added tank book code example

--- build/premake/examples.lua
+++ build/premake/examples.lua
@@ -3,7 +3,7 @@
 -- examples
 --
 
--- checkout lineSimple cockpit
+-- checkout line
 project "checkout_line"
    kind "ConsoleApp"
    targetname "checkout_line"
@@ -12,6 +12,25 @@ project "checkout_line"
    files {
       "../../examples/checkout_line/**.h*",
       "../../examples/checkout_line/**.cpp"
+   }
+   includedirs { ADEVS_IncPath }
+   if _ACTION ~= "gmake" then
+      defines { "_CONSOLE" }
+   end
+
+--
+-- examples (book-code)
+--
+
+-- tank
+project "tank"
+   kind "ConsoleApp"
+   targetname "tank"
+   targetdir "../../examples/book-code/tank"
+   debugdir "../../examples/book-code/tank"
+   files {
+      "../../examples/book-code/tank/**.h*",
+      "../../examples/book-code/tank/**.cpp"
    }
    includedirs { ADEVS_IncPath }
    if _ACTION ~= "gmake" then
